0baff293d57ad8c7ea1cad7483e8fbac30f2979d
aristos <aristosmiliaresis@gmail.com>
2025-01-24 12:03:23 +0200

    .

--- conf/ansible/roles/interactsh/files/start_interactsh.sh
+++ conf/ansible/roles/interactsh/files/start_interactsh.sh
@@ -13,7 +13,7 @@ ip=$(curl -s ifconfig.me)
 token=$(cat /dev/random | head -c 48 | base64 | tr -d '=')
 
 interactsh-server -eviction 0 -hi /opt/www/index.html -hd /opt/www -scan-everywhere -dynamic-resp -d $DOMAIN \
-    -smb -http-port 8 -https-port 4 -ip $ip -server-header nginx -disable-version -wildcard -t "$token" &
+    -smb -ldap -http-port 8 -https-port 4 -ip $ip -server-header nginx -disable-version -wildcard -t "$token" &
 
 sleep 5
 
@@ -27,20 +27,20 @@ do
     cat /tmp/interaction.tmp \
         | jq -c 'select( .protocol == "http" ) | {"remote-address",asninfo,"raw-request"}'| awk '!x[$0]++' \
         | jq -r '"\(."remote-address") \(.asninfo[0].org)\n\(."raw-request")"' \
-        | notify -silent -bulk -provider discord -id http
+        | notify -pc /opt/provider-config.yaml -silent -bulk -provider discord -id http
 
     cat /tmp/interaction.tmp \
         | jq -c 'select( .protocol == "dns" )' \
         | jq -r '"\(."remote-address") \(.asninfo[0].org)\n\(."raw-request")"' \
-        | notify -silent -bulk -provider discord -id dns
+        | notify -pc /opt/provider-config.yaml -silent -bulk -provider discord -id dns
 
     cat /tmp/interaction.tmp \
         | jq -c 'select( .protocol == "smtp" )' \
         | jq -r '"\(."remote-address") \(.asninfo[0].org)\n\(."raw-request")"' \
-        | notify -silent -bulk -provider discord -id smtp
+        | notify -pc /opt/provider-config.yaml -silent -bulk -provider discord -id smtp
 
     cat /tmp/interaction.tmp \
         | jq -c 'select( .protocol == "smb" )' \
         | jq -r '."raw-request"' \
-        | notify -silent -bulk -provider discord -id smb
+        | notify -pc /opt/provider-config.yaml -silent -bulk -provider discord -id smb
 done
